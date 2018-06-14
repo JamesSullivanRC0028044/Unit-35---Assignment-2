c69c450161ae614427bfad0d8eb8e4dbab6ff57b
James Sullivan <rc0028044@student.riversidecollege.ac.uk>
2018-06-14 09:56:42 +0100

    Add project files.

---
+++ Unit 35 - Assignment 2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Unit_35___Assignment_2
+{
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            public double time;
+            public double altitude;
+            public double velocity;
+            public double acceleration;
+        }
+
+        List<row> table = new List<row>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CalculateVelocity()
+        {
+            for (int i = 1; i < table.Count; i++)
+            {
+                double dQ = table[i].altitude - table[i - 1].altitude;
+                double dt = table[i].time - table[i - 1].time;
+                table[i].velocity = dQ / dt;
+
+            }
+        }
+
+        private void CalculateAcceleration()
+        {
+            for (int i = 2; i < table.Count; i++)
+            {
+                double dV = table[i].velocity - table[i - 1].velocity;
+                double dt = table[i].time - table[i - 1].time;
+                table[i].acceleration = dV / dt;
+
+            }
+        }
+
+
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] r = sr.ReadLine().Split(',');
+                            table.Last().time = double.Parse(r[0]);
+                            table.Last().altitude = double.Parse(r[1]);
+                        }
+                    }
+                    CalculateVelocity();
+                    CalculateAcceleration();
+
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+                catch (DivideByZeroException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " has rows that have the same time");
+                }
+            }
+
+        }
+    }
+}
